314bde5587adf44edcc575cf8bfe55c8f48a28ed
kasthuriraajan <kasthuriraajan94@gmail.com>
2017-07-18 20:56:14 +0530

    Springbootinitialization
    Here our project is created in springboot with thymeleve. index.html is created

---
+++ TriCon/src/main/java/TriCon/controller/indexController.java
@@ -0,0 +1,25 @@
+package TriCon.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+
+@Controller
+public class indexController {
+
+	// inject via application.properties
+	@Value("${index.message:test}")
+	private String message = "Hello World";
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "index";
+	}
+
+}
+
