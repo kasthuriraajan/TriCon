13a33be03041a0d3c38ceb54bef832f94982bcc1
kasthuriraajan <kasthuriraajan94@gmail.com>
2017-07-27 18:24:29 +0530

    industrial part initialization
    I add some industrialist's pages and fragments header footer, sidebar, userdetailsetc

--- TriCon/src/main/java/TriCon/controller/indexController.java
+++ TriCon/src/main/java/TriCon/controller/indexController.java
@@ -18,13 +18,38 @@ public class indexController {
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
 		model.put("message", this.message);
-		return "index";
+		return "fragments/index";
 	}
 
 	@RequestMapping("/weeklyreport")
 	public String weeklyreport(Map<String, Object> model) {
 		model.put("message", this.message);
 		return "weeklyreport";
+	}
+	@RequestMapping("/header")
+	public String header(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "fragments/header";
+	}
+	@RequestMapping("/footer")
+	public String footer(Map<String, Object> model) {
+
+		return "fragments/footer";
+	}
+	@RequestMapping("/profile")
+	public String profile(Map<String, Object> model) {
+
+		return "fragments/profile";
+	}
+	@RequestMapping("/userlist")
+	public String userlist(Map<String, Object> model) {
+
+		return "fragments/userlist";
+	}
+	@RequestMapping("/sidebar")
+	public String sidebar(Map<String, Object> model) {
+
+		return "fragments/sidebar";
 	}
 }
 
