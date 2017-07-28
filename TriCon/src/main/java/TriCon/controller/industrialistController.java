f67d792678a5420a031257b0ef93209f7b22d124
kasthuriraajan <kasthuriraajan94@gmail.com>
2017-07-28 10:45:35 +0530

    profileupdatepage

--- TriCon/src/main/java/TriCon/controller/industrialistController.java
+++ TriCon/src/main/java/TriCon/controller/industrialistController.java
@@ -4,18 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
+
 @Controller
 public class industrialistController {
-    @RequestMapping("/ind/ index")
+    @RequestMapping("/ind/index")
     public String index(Map<String, Object> model) {
         return "industrialist/index";
     }
+
     @RequestMapping("/ind/commitedstudents")
     public String commitedstudents(Map<String, Object> model) {
         return "industrialist/commitedstudents";
     }
+
     @RequestMapping("/ind/weeklyreport")
     public String weeklyreport(Map<String, Object> model) {
         return "industrialist/weeklyreport";
+    }
+
+    @RequestMapping("/ind/profileupdate")
+    public String profileupdate(Map<String, Object> model) {
+        return "industrialist/profileupdate";
     }
 }
