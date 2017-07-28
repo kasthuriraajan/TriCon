a29b0c76ea04a990d4aa423c30a941888c13816e
tharmini <tharmini7@gmail.com>
2017-07-28 21:44:33 +0530

    fyy

--- TriCon/src/main/java/TriCon/controller/lecturerController.java
+++ TriCon/src/main/java/TriCon/controller/lecturerController.java
@@ -1,7 +1,9 @@
 package TriCon.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
 
 
 @Controller
