6b8a76fa5482fbeb8b167bda5c47a3de5c11b5fa
kasthuriraajan <kasthuriraajan94@gmail.com>
2017-07-29 13:23:59 +0530

    add for correcting error

--- TriCon/src/main/java/TriCon/controller/lecturerController.java
+++ TriCon/src/main/java/TriCon/controller/lecturerController.java
@@ -1,7 +1,9 @@
 package TriCon.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
 
 
 @Controller
@@ -12,29 +14,29 @@ public class lecturerController {
     {
         return "lecturer/index";
     }
+
     @RequestMapping("/lec/inspectreport")
-    public String inspectreport(Map<String, Object> model)
-    {
+    public String inspectreport(Map<String, Object> model) {
         return "lecturer/inspectreport";
     }
+
     @RequestMapping("/lec/weeklyreport")
-    public String weeklyreport(Map<String, Object> model)
-    {
+    public String weeklyreport(Map<String, Object> model) {
         return "lecturer/weeklyreport";
     }
+
     @RequestMapping("/lec/weeklyreportedit")
-    public String weeklyreportedit(Map<String, Object> model)
-    {
+    public String weeklyreportedit(Map<String, Object> model) {
         return "lecturer/weeklyreportedit";
     }
+
     @RequestMapping("/lec/inspectreportedit")
-    public String inspectreportedit(Map<String, Object> model)
-    {
+    public String inspectreportedit(Map<String, Object> model) {
         return "lecturer/inspectreportedit";
     }
+
     @RequestMapping("/lec/pastrecords")
-    public String pastrecords(Map<String, Object> model)
-    {
+    public String pastrecords(Map<String, Object> model) {
         return "lecturer/pastrecords";
     }
 }
