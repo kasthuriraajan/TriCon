b328b41b1b298dcc51d5d6e1562e44f6435770d0
Meruja <merujajoseph@gmail.com>
2017-08-20 19:23:49 +0530

    model for student

---
+++ TriCon/src/main/java/TriCon/model/weeklyReport.java
@@ -0,0 +1,177 @@
+package TriCon.model;
+
+public class weeklyReport
+{
+    private String weeklyReportId;
+    private String journalId;
+    private String from;
+    private String to;
+    private String monday;
+    private String tuesday;
+    private String wednesday;
+    private String thursday;
+    private String friday;
+    private String saturday;
+    private String sunday;
+    private String summary;
+    private String attachment;
+    private String review;
+    private String remarks;
+    private String status;
+    private String studentSignature;
+    private String lecturerSignature;
+    private String industrialistSignature;
+
+
+    public String getJournalId() {
+        return journalId;
+    }
+
+    public void setJournalId(String journalId) {
+        this.journalId = journalId;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getMonday() {
+        return monday;
+    }
+
+    public void setMonday(String monday) {
+        this.monday = monday;
+    }
+
+    public String getTuesday() {
+        return tuesday;
+    }
+
+    public void setTuesday(String tuesday) {
+        this.tuesday = tuesday;
+    }
+
+    public String getWednesday() {
+        return wednesday;
+    }
+
+    public void setWednesday(String wednesday) {
+        this.wednesday = wednesday;
+    }
+
+    public String getThursday() {
+        return thursday;
+    }
+
+    public void setThursday(String thursday) {
+        this.thursday = thursday;
+    }
+
+    public String getFriday() {
+        return friday;
+    }
+
+    public void setFriday(String friday) {
+        this.friday = friday;
+    }
+
+    public String getSaturday() {
+        return saturday;
+    }
+
+    public void setSaturday(String saturday) {
+        this.saturday = saturday;
+    }
+
+    public String getSunday() {
+        return sunday;
+    }
+
+    public void setSunday(String sunday) {
+        this.sunday = sunday;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStudentSignature() {
+        return studentSignature;
+    }
+
+    public void setStudentSignature(String studentSignature) {
+        this.studentSignature = studentSignature;
+    }
+
+    public String getLecturerSignature() {
+        return lecturerSignature;
+    }
+
+    public void setLecturerSignature(String lecturerSignature) {
+        this.lecturerSignature = lecturerSignature;
+    }
+
+    public String getIndustrialistSignature() {
+        return industrialistSignature;
+    }
+
+    public void setIndustrialistSignature(String industrialistSignature) {
+        this.industrialistSignature = industrialistSignature;
+    }
+
+    public String getWeeklyReportId() {
+        return weeklyReportId;
+    }
+
+    public void setWeeklyReportId(String weeklyReportId) {
+        this.weeklyReportId = weeklyReportId;
+    }
+}
