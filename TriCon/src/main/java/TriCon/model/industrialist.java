beebb7bd6b32957e5bd041d255ac78ac281a0bb4
romipraveen1 <romipraveen100@gmail.com>
2017-08-31 12:53:30 +0530

    industrialist model

---
+++ TriCon/src/main/java/TriCon/model/industrialist.java
@@ -0,0 +1,153 @@
+package TriCon.model;
+
+public class industrialist {
+
+
+
+    private String IndId;
+    private String LastName;
+    private String Email;
+    private String Address;
+
+    private String Linkedin;
+    private String Twitter;
+    private String Github;
+    private String Facebook;
+    private String Blog;
+    private int MobileNo;
+    private int TeleNo;
+    private String IndSign;
+    private String FirstName;
+    private String Company;
+    private String CompanyAddress;
+    private String Designation;
+
+
+    public String getIndId() {
+        return IndId;
+    }
+
+    public void setIndId(String indId) {
+        IndId = indId;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getLinkedin() {
+        return Linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        Linkedin = linkedin;
+    }
+
+    public String getTwitter() {
+        return Twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        Twitter = twitter;
+    }
+
+    public String getGithub() {
+        return Github;
+    }
+
+    public void setGithub(String github) {
+        Github = github;
+    }
+
+    public String getFacebook() {
+        return Facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        Facebook = facebook;
+    }
+
+    public String getBlog() {
+        return Blog;
+    }
+
+    public void setBlog(String blog) {
+        Blog = blog;
+    }
+
+    public int getMobileNo() {
+        return MobileNo;
+    }
+
+    public void setMobileNo(int mobileNo) {
+        MobileNo = mobileNo;
+    }
+
+    public int getTeleNo() {
+        return TeleNo;
+    }
+
+    public void setTeleNo(int teleNo) {
+        TeleNo = teleNo;
+    }
+
+    public String getIndSign() {
+        return IndSign;
+    }
+
+    public void setIndSign(String indSign) {
+        IndSign = indSign;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getCompany() {
+        return Company;
+    }
+
+    public void setCompany(String company) {
+        Company = company;
+    }
+
+    public String getCompanyAddress() {
+        return CompanyAddress;
+    }
+
+    public void setCompanyAddress(String companyAddress) {
+        CompanyAddress = companyAddress;
+    }
+
+    public String getDesignation() {
+        return Designation;
+    }
+
+    public void setDesignation(String designation) {
+        Designation = designation;
+    }
+}
